4fde970629733e7bb8fd85191be3f691b988fcad
g-siddharthareddy123 <siddharthareddyganta@gmail.com>
2025-12-07 23:41:31 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -7,7 +7,7 @@ import "./Login.css";
 import showIcon from "../assets/show.png";
 import hideIcon from "../assets/hidden.png";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export default function Register() {
   const [name, setName] = useState("");
